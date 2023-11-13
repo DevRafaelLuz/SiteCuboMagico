11c5a3d8660427098297a3a3d25a7c7afac9e6b1
RafaelLuz <rafael.luz2005@gmail.com>
2023-11-13 19:39:06 -0300

    Configuração inicial

---
+++ src/main/java/com/senacead/cuboMagico/CuboMagicoApplication.java
@@ -0,0 +1,13 @@
+package com.senacead.cuboMagico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuboMagicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuboMagicoApplication.class, args);
+	}
+
+}
