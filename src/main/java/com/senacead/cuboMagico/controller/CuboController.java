37dc2fb7294439b17514d81f0d4e8b1d6b2a2a04
RafaelLuz <rafael.luz2005@gmail.com>
2023-11-15 16:12:17 -0300

    Classe usuário pronta e alguns ajustes no formulário

--- src/main/java/com/senacead/cuboMagico/controller/CuboController.java
+++ src/main/java/com/senacead/cuboMagico/controller/CuboController.java
@@ -1,10 +1,15 @@
 package com.senacead.cuboMagico.controller;
 
+import com.senacead.cuboMagico.model.Usuario;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class CuboController {
+public class CuboController {   
     @GetMapping("/pagina-principal")
     public String mostrarPagPrincipal() {
         return "index";
@@ -24,7 +29,7 @@ public class CuboController {
         return "pecasCubo";
     }
     
-    @GetMapping("metodo-basico")
+    @GetMapping("/metodo-basico")
     public String exibeMetodo() {
         return "metodoBasico";
     }
---
+++ src/main/java/com/senacead/cuboMagico/model/Usuario.java
@@ -0,0 +1,25 @@
+package com.senacead.cuboMagico.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Usuario")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    
+    private String nome;
+    private String telefone;
+    private String email;
+}
